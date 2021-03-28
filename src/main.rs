d8eadc206428a434bfb40e3735340fd3bf50d471
Yujeonja <hardboiled65@gmail.com>
2021-03-28 17:04:59 +0900

    Properties API error with Rust Rocket.

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+
+use rocket::http::{ContentType, Status};
+use rocket::request::Request;
+use rocket::response;
+use rocket::response::{Responder, Response};
+use rocket_contrib::json::{Json, JsonValue};
+
+
+#[derive(Debug)]
+struct ApiResponse {
+    json: JsonValue,
+    status: Status,
+}
+
+impl<'r> Responder<'r> for ApiResponse {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        Response::build_from(self.json.respond_to(&req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
+
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/echo/<num>")]
+fn echo(num: String) -> ApiResponse {
+    if num == "1" {
+        ApiResponse {
+            json: json!({"error": "first error!"}),
+            status: Status::NotFound,
+        }
+    } else {
+        ApiResponse {
+            json: json!({"ok": "done!"}),
+            status: Status::Ok,
+        }
+    }
+}
+
+#[get("/api/unicode/properties/<cp>")]
+fn properties(cp: String) -> ApiResponse {
+    let cp = format!("{}", cp);
+    let cp = u32::from_str_radix(&cp, 16);
+    match cp {
+        Ok(val) => {
+            if val < 0 || val > 0x10FFFF {
+                return ApiResponse {
+                    json: json!({"message": "The Unicode number is out of range"}),
+                    status: Status::NotFound,
+                };
+            }
+            ApiResponse {
+                json: json!({"message": "Ok"}),
+                status: Status::Ok,
+            }
+        },
+        Err(e) => {
+            ApiResponse {
+                json: json!({"message": format!("{}", e)}),
+                status: Status::BadRequest,
+            }
+        },
+    }
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![hello, properties]).launch();
+}
